489a7ea5fd9330539e3c2b7174bdfcd6be6329fb
Ali Eslami <indigoae@hotmail.com>
2012-10-23 04:43:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapper::Application.config.secret_token = '7d21b7825ae1ddea359dddd9a114a3c4434423a96151d83908d621ff163b52f9f0e911467d4660ef40abedcbd40c0313bb80f1e91ddc008a67816cd9fb920e82'
